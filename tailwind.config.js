a07056e4daf7b85810a8d8faebea083bbed34dff
Muhammad lutfi <sainteclutfi@gmail.com>
2022-10-09 20:44:17 +0800

    update responsive with grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["ui-sans-serif", "system-ui"],
+    },
   },
   plugins: [],
 };
